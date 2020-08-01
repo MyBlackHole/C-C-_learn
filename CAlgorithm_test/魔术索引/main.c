83ed1288880a26c0c9c245be689c130d51149aba
Black Hole <1358244533@qq.com>
2020-08-01 10:15:12 +0800

    traverse

--- CAlgorithm_test/魔术索引/main.c
+++ CAlgorithm_test/魔术索引/main.c
@@ -2,7 +2,6 @@
 // Created by BlackHole on 2020/8/1.
 //
 #include "stdio.h"
-#include "string.h"
 
 int getAnswer(int *nums, int left, int right) {
     if (left > right) {
@@ -22,10 +21,20 @@ int findMagicIndex(int *nums, int numsSize) {
     return getAnswer(nums, 0, numsSize - 1);
 }
 
+int leftTraverse(int *nums, int numsSize) {
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] == i) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 int main() {
-    int is[] = {6, 2, 4, 3, 1, 8};
+    int is[] = {6, 1, 4, 3, 1, 8};
     int *nums = is;
-    int i = findMagicIndex(nums, 6);
+//    int i = findMagicIndex(nums, 6);
+    int i = leftTraverse(nums, 6);
     printf("%d", i);
 }
 
