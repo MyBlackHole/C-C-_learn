c8c5e2aac27e9a8ecdc3973164ad0a8289ca8b96
Black Hole <1358244533@qq.com>
2020-09-16 23:14:19 +0800

    -

--- 解密/main.c
+++ 解密/main.c
@@ -1,16 +1,11 @@
 //
 // Created by BlackHole on 2020/7/22.
 //
-// Building a simple rotation cipher.  This is just practice
-// since my c/c++ skillz are lacking again.
-// author: Nick Soracco <nsoracco!AT!cs.cur.edu>
-// Licence: public domain
 
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-// pull in a single char and a shift value
 void rotate(char *, int);
 
 int main(int argc, char **argv) {
@@ -28,17 +23,8 @@ int main(int argc, char **argv) {
     printf("subject: %s Shift: %d\n", argv[2], shift);
     rotate(argv[2], shift);
     printf("Ciphertext: %s\n", argv[2]);
-
 }
 
-// By reducing the symbol's ASCII value to it's symbolic from 0 representation
-// and then adding the shift value and modulo by the number of symbols in the
-// alphabet and then adding the base back in to get the ASCII value of the
-// shifted value back.  The easier way (and works with all symbols!) is to do
-// something like this: shifted = (((symbol + shift) % symbol ) + offset)
-// where:	offset = distance from 0 to the first symbol's ascii value
-// 		symbol = ascii value of the symbol in question
-//		shift  = how many places up to move.
 void rotate(char *cleartext, int shift) {
     int length = strlen(cleartext);
     char temp;
