8fb5c4ce1c18ddc50940e3edf28b30511512fdd1
Black Hole <1358244533@qq.com>
2020-10-21 22:17:11 +0800

    setenb

--- stdlib_learn/api.c
+++ stdlib_learn/api.c
@@ -8,6 +8,14 @@
 int main() {
     char s[10] = {'P', 'A', 'T', 'H'};
     char *name = s;
-    printf("%s\n", getenv(name));
+
+//    // 获取环境变量
+//    printf("%s\n", getenv(name));
+
+//    // 修改环境变量
+//    printf("%s\n", getenv(name));
+//    setenv("PATH", "hello", 1)
+//    printf("%s\n", getenv(name));
+
     return 0;
 }
