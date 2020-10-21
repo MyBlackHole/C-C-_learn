a8fc140cae554a50ea3aa47d59d241eb5fa92e9c
black hole <1358244533@qq.com>
2020-10-21 23:45:30 +0800

    waitpid

--- stdlib_learn/api.c
+++ stdlib_learn/api.c
@@ -12,10 +12,10 @@ int main() {
 //    // 获取环境变量
 //    printf("%s\n", getenv(name));
 
-//    // 修改环境变量
-//    printf("%s\n", getenv(name));
-//    setenv("PATH", "hello", 1)
-//    printf("%s\n", getenv(name));
+    // 修改环境变量
+    printf("%s\n", getenv(name));
+    setenv("PATH", "hello", 1);
+    printf("%s\n", getenv(name));
 
     return 0;
 }
