36c360e82495bb699fd62bc16c07211500ac047f
Black Hole <1358244533@qq.com>
2020-11-12 23:04:13 +0800

    arm

--- arm/main.c
+++ arm/main.c
@@ -1,6 +1,13 @@
 //
 // Created by BlackHole on 2020/7/15.
 //
+#include <stdio.h>
 
-
+int main() {
+    int input = 8;
+    int result = 0;
+    __asm__ __volatile__ ("movl %1,%0" : "=r" (result) : "r" (input));
+    printf("%d\n", result);
+    return 0;
+}
 
