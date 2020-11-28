13a302e4764817abc2060b3be99bcc788c662132
Black Hole <1358244533@qq.com>
2020-11-28 22:00:15 +0800

    语法

--- 语法/1.c
+++ 语法/1.c
@@ -5,18 +5,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int func();
+/*
+ * 1
+ * 逻辑 True ? 1: 2
+ *
+ * 2
+ * |=
+ *
+ * 3
+ * &=
+ *
+ * 4
+ * ~
+ *
+ * 5
+ * 十进制、八进制、十六进制
+ *
+ */
 
-int main() {
-    func();
-    exit(0);
+
+int func5() {
+    int i = 1000;
+    printf("八进制:%o\n", i);
+    printf("十进制:%d\n", i);
+    printf("十六进制:%X\n", ~0);
+    return 0;
 }
 
-/*
- * 逻辑 True ? 1: 2
- */
-int func() {
+int func4() {
+    int i = 0;
+    printf("%d", ~i);
+    return 0;
+}
+
+int func3() {
+    int i = 0;
+    i &= 1 << 2;
+    printf("%d", i);
+    return 0;
+}
+
+int func2() {
+    int i = 0;
+    i |= 1 << 2;
+    printf("%d", i);
+    return 0;
+}
+
+
+int func1() {
     printf("%d", 0 ? 1 : 2);
     return 0;
 }
 
+
+int main() {
+    func5();
+    exit(0);
+}
